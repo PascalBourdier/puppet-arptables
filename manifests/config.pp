2646504bfd2f26da16ea6431a6678e0ceba2ab19
Vanessa Novak <vnovak@hetcak.nl>
2015-04-13 15:19:00 +0200

    fixed arptables config.pp to be dependent on service status

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,7 @@
 class arptables::config {
 
+  unless $::arptables::service_enable == false {
+
     file { $::arptables::arp_packetfilter:
       ensure  => 'file',
       content => epp('arptables/arptables.epp'),
@@ -9,36 +11,37 @@ class arptables::config {
       notify  => Class['arptables::service'],
     }
 
-if $::arptables::manage_ip_alias == true {
+    if $::arptables::manage_ip_alias == true {
 
-    # register yourself in rclocal
-    ::rclocal::register{ 'arptables':
-      content   => epp('arptables/add_ipalias.epp'),
-      subscribe => File[$::arptables::arp_packetfilter],
-    }
-
-    case $::arptables::virtual_ip {
-      String : {
-        exec { "run_ip_add_${::arptables::virtual_ip}":
-          command     => "/sbin/ip addr add ${::arptables::virtual_ip} dev ${::arptables::interface}",
-          unless      => "/sbin/ip -4 a | grep ${::arptables::virtual_ip}",
-          subscribe   => File[$::arptables::arp_packetfilter],
-          refreshonly => true,
-        }
+      # register yourself in rclocal
+      ::rclocal::register{ 'arptables':
+        content   => epp('arptables/add_ipalias.epp'),
+        subscribe => File[$::arptables::arp_packetfilter],
       }
-      Array : {
-        each($::arptables::virtual_ip) |$value| {
-          exec { "run_ip_add_${value}":
-            command     => "/sbin/ip addr add ${value} dev ${::arptables::interface}",
-            unless      => "/sbin/ip -4 a | grep ${value}",
+
+      case $::arptables::virtual_ip {
+        String : {
+          exec { "run_ip_add_${::arptables::virtual_ip}":
+            command     => "/sbin/ip addr add ${::arptables::virtual_ip} dev ${::arptables::interface}",
+            unless      => "/sbin/ip -4 a | grep ${::arptables::virtual_ip}",
             subscribe   => File[$::arptables::arp_packetfilter],
             refreshonly => true,
           }
         }
-      }
-      default: {
-        fail('The $::arptables::virtual_ip is of type that is not supported')
+        Array : {
+          each($::arptables::virtual_ip) |$value| {
+            exec { "run_ip_add_${value}":
+              command     => "/sbin/ip addr add ${value} dev ${::arptables::interface}",
+              unless      => "/sbin/ip -4 a | grep ${value}",
+              subscribe   => File[$::arptables::arp_packetfilter],
+              refreshonly => true,
+            }
+          }
+        }
+        default: {
+          fail('The $::arptables::virtual_ip is of type that is not supported')
+        }
       }
     }
-}
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,12 @@
 # Userspace control program for the arptables network filter
 class arptables (
   Boolean $service_enable = false,
-  $service_ensure = 'stopped',
-  $virtual_ip = undef,
-  $real_ip = undef,
-  $virtual_real_ip = undef,
-  $arp_packetfilter = '/etc/sysconfig/arptables',
-  $interface = undef,
+  String $service_ensure = 'stopped',
+  Variant[Undef, String, Array] $virtual_ip = undef,
+  Variant[Undef, String] $real_ip = undef,
+  Variant[Undef, Hash] $virtual_real_ip = undef,
+  String $arp_packetfilter = '/etc/sysconfig/arptables',
+  Variant[Undef, String] $interface = undef,
   Boolean $manage_ip_alias = true,
 
   ) {
@@ -17,4 +17,3 @@ contain arptables::install
 contain arptables::config
 contain arptables::service
 }
-
